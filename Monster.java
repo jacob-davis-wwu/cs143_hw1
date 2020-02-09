7481eae34f4320a0251cc92aa71f278a6b032fb3
Ben-Fritz <59749709+Ben-Fritz@users.noreply.github.com>
2020-02-09 12:45:54 -0800

    Add files via upload

--- Monster.java
+++ Monster.java
@@ -1,14 +1,56 @@
 
+import java.util.*;
 public class Monster {
+	/*
+	 * This is the main Monster Class. This class contains the 
+	 * settings and controls for the different types of monsters
+	 * that the player will be fighting.
+	 * */
     private int health;
     private int damage;
     private String monsterType;
+    private String name;
 
+    public Monster(String monsterVar) {
+    	monsterVar.toUpperCase();
+    	if(monsterVar.equals("ORC")) {
+    		this.health = 18;
+    		this.damage = 20;
+    		this.name = "Orc";
+    	}
+    	else if(monsterVar.equals("GOBLIN")){
+    		this.health = 6;
+    		this.damage = 10;
+    		this.name = "Goblin";
+    	}
+    	else if(monsterVar.equals("ZOMBIE")) {
+    		this.health = 12;
+    		this.damage = 15;
+    		this.name = "Zombie";
+    	}
+    	else if(monsterVar.equals("DENEKE")){
+    		this.health = 55;
+    		this.damage = 5;
+    		this.name = "Deneke";
+    	}
+    	else {
+    		this.damage = 0;
+    		this.health = 0;
+    		this.name = "";
+    	}
+    }
     public void attack(Player target) {
-
+    	target.onHit(this.damage);
     }
 
     public void onHit(int damage) {
-
+    	this.health -= damage;
+    }
+    public String toString() {
+    	return this.name;
+    }
+    public void main(String[] args) {
+    	Monster orc1 = new Monster("Orc");
+    	System.out.println(orc1);
     }
 }
