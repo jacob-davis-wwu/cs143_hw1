9ae27940dd2cae126d2482665fe9b4cdebc3520f
jacob-davis-wwu <davisj52@wwu.edu>
2020-02-16 11:59:35 -0800

    added player movement methods to DungeonMap class

--- DungeonMap.java
+++ DungeonMap.java
@@ -22,6 +22,46 @@ public class DungeonMap {
         }
     }
 
+    // Moving the player
+    // Each movement method returns a boolean based on whether or not it was
+    // possible to move in the desired direction
+
+    public boolean moveUp() {
+        if ( playerLocation[1] != 0 ) {
+            playerLocation[1] -= 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean moveDown() {
+        if ( playerLocation[1] != rows - 1 ) {
+            playerLocation[1] += 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean moveLeft() {
+        if ( playerLocation[0] != 0 ) {
+            playerLocation[0] -= 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean moveRight() {
+        if ( playerLocation[0] != cols - 1 ) {
+            playerLocation[0] += 1;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
     // Printing the map
 
     public void print() {
@@ -36,7 +76,7 @@ public class DungeonMap {
                         row = row + "W";
                     }
                 } else if (rooms[i][j].hasVisited()) {
-                    row = row + "x";
+                    row = row + "*";
                 } else {
                     row = row = " ";
                 }
